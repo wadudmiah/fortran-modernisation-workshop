dbcd9cabaa2f7a9ffd799bac41be4cd938115120
Wadud Miah <wadud.miah@gmail.com>
2016-02-03 12:38:49 +0000

    moved func to RHS_mod.f90 and added rule for it in Makefile

---
+++ solutions/RHS_mod.f90
@@ -0,0 +1,17 @@
+module RHS_mod
+
+  use Types_mod
+  
+  implicit none
+
+contains
+  
+  function func( j, x_num, x ) result ( d )
+    integer(KIND=SI), intent(in) :: j, x_num
+    real(KIND=DP) :: d
+    real(KIND=DP), intent(in) :: x(x_num)
+
+    d = 0.0_DP
+  end function func
+  
+end module RHS_mod
