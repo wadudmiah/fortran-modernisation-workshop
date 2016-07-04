6bea7c3e7a52b4807f1ca66bd78f7d12dd159d4e
Wadud Miah <wadud.miah@nag.co.uk>
2016-07-04 10:14:00 +0100

    added fortran usage statistics script

---
+++ fortra_usage/fortran_usage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import sys
+import optparse
+import numpy as np
+import matplotlib as mpl
+mpl.use( 'Agg' )
+import matplotlib.pyplot as plt
+
+parser = optparse.OptionParser( )
+
+parser.add_option( '--input_file', dest = 'input_file', default = '' )
+parser.add_option( '--output_file', dest = 'output_file', default = 'output.png' )
+parser.add_option( '--title', dest = 'title', default = 'Fortran usage' )
+
+options, remainder = parser.parse_args( )
+
+data = np.genfromtxt( options.input_file, dtype = [ ( 'fortran_standard', 'a4' ), \
+                                                    ( 'fortran_usage', 'i8' ), ], \
+                                          comments = '#' )
+
+fortran_standard = data['fortran_standard']
+fortran_usage = data['fortran_usage']
+
+xrange = np.arange( 0, fortran_standard.size )
+labels = fortran_standard
+width = 0.50
+total_usage = np.sum( fortran_usage )
+
+fig, plt1 = plt.subplots( )
+
+l1 = plt1.bar( xrange, fortran_usage, label = 'off', color = 'blue' )
+plt1.set_ylabel( 'Usage' )
+plt1.set_xlabel( 'Fortran standard' )
+plt1.grid( True )
+
+# plt2 = plt1.twinx( )
+# l3 = plt2.bar( xrange + 2*width, diff_data, width = width, label = 'diff', color = 'red' )
+# plt2.set_ylabel( 'relative diff' )
+# plt2.set_ylim( [ -0.06, 0.06 ] )
+# plt2.grid( True )
+
+# for t2 in plt2.get_yticklabels( ):
+#  t2.set_color( 'red' )
+
+# plt.legend( ( l1, l2, l3 ), ( 'off', 'on', 'diff' ), loc = 'upper right' )
+plt.axis( xmin = -0.4, xmax = fortran_usage.size )
+plt.xticks( xrange + 0.4, fortran_standard )
+plt.title( options.title )
+plt.savefig( options.output_file )
+ 
